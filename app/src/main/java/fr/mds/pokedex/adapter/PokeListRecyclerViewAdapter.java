9aa5b1e21b4a16e42479244a769aa8e8b66eca13
nl2br <natleb7@gmail.com>
2019-04-30 09:25:27 +0200

    exercice endless scroll

--- app/src/main/java/fr/mds/pokedex/adapter/PokeListRecyclerViewAdapter.java
+++ app/src/main/java/fr/mds/pokedex/adapter/PokeListRecyclerViewAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import fr.mds.pokedex.R;
 import fr.mds.pokedex.viewHolder.PokeListViewHolder;
@@ -16,33 +17,36 @@ import fr.mds.pokedex.model.PokeCard;
 public class PokeListRecyclerViewAdapter extends RecyclerView.Adapter<PokeListViewHolder> {
 
     private ArrayList<PokeCard> pokeCards;
-    private View.OnClickListener onClickListener;
+    //private View.OnClickListener onClickListener;
     public static final String TAG = "pokedex";
 
     public PokeListRecyclerViewAdapter(ArrayList<PokeCard> pokeCards){
         this.pokeCards = pokeCards;
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull final PokeListViewHolder holder,final int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "item clicked " + pokeCards.get(holder.getAdapterPosition()).getName());
+               // onClickListener.onClick(v);
+            }
+        });
+    }
+
     @NonNull
     @Override
     public PokeListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         PokeListViewHolder pokeListViewHolder = new PokeListViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false));
-//        pokeListViewHolder.itemView.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(TAG, "onclick " + v);
-//                onClickListener.onClick(v);
-//            }
-//        });
-
         return pokeListViewHolder;
     }
 
-    public void setOnClickListener(View.OnClickListener onClickListener){
-        Log.d(TAG, "click " + onClickListener);
-        this.onClickListener = onClickListener;
-    }
+//    public void setOnClickListener(View.OnClickListener onClickListener){
+//        Log.d(TAG, "click " + onClickListener);
+//        this.onClickListener = onClickListener;
+//    }
 
     @Override
     public void onBindViewHolder(@NonNull PokeListViewHolder holder, int position) {
